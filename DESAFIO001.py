1b118707c90255061ecae075a7ffaa978308c210
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-06-18 17:09:58 -0300

    Atividades de Python realizada pelo Curso em Vídeo

---
+++ DESAFIO001.py
@@ -0,0 +1,5 @@
+#Crie um programa que escreva "Olá, Mundo!" na tela.
+print('='*30, 'DESAFIO 001', '='*30)
+
+msg = 'Olá, Mundo!'
+print(msg)
---
+++ DESAFIO002.py
@@ -0,0 +1,9 @@
+'''Faça um programa que leia o nome de uma pessoa e mostre uma
+mensagem de boas-vindas'''
+
+print('='*30, 'DESAFIO 002', '='*30)
+
+nome = str(input('Digite seu nome: '))
+print(f'É um prazer te conhecer, {nome}!')
+
+
