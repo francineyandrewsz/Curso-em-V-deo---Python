768af17aff0fb5a7c5befdac4f4ff145c02ab463
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-06-21 17:28:14 -0300

    Add files via upload

---
+++ DESAFIO003.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+print('='*30, 'DESAFIO 003', '='*30)
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+print(f'A soma entre {n1} e {n2} é igual a {n1+n2}')
+
