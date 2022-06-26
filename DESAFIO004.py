d07e0f2c7455cd17bfd20030452cb624575c7485
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-06-25 23:41:02 -0300

    Dissecando uma Variável
    Atividade do Curso de Python criado pelo professor Gustavo Guanabara para o portal CursoemVideo.com.

--- DESAFIO004.py
+++ DESAFIO004.py
@@ -1,3 +1,6 @@
+'''Faça um programa que leia algo pelo teclado e mostre na tela
+o seu tipo primitivo e todas as informaçoes possíveis sobre ele.'''
+
 a = input('Digite algo: ')
 print('O tipo primitivo desse valor é', type(a))
 print('Só tem espaços?', a.isspace())
