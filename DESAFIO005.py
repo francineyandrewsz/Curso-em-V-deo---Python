6b064e1389393d3473f0032049be89dc3e266bd9
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-06-29 21:57:48 -0300

    Add files via upload
    Um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.

---
+++ DESAFIO005.py
@@ -0,0 +1,6 @@
+'''Faça um programa que leia um número inteiro e mostre na tela o seu
+sucessor e seu antecessor.'''
+
+print('=>'*20, 'DESAFIO005', '<='*20)
+n = int(input('Digite um número: '))
+print(f'Analisando o valor {n}, seu antecessor é {n-1} e o sucessor é {n+1}')
