531af55d75d8e2db44039297333856f94d645f12
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-06-29 22:10:20 -0300

    Add files via upload
    Dobro, Triplo, Raiz Quadrada.

---
+++ DESAFIO006.py
@@ -0,0 +1,9 @@
+'''Crie um algoritmo que leia um número e mostre o seu dobro, triplo e
+raiz quadrada.'''
+
+print('=>'*20, 'DESAFIO006', '<='*20)
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} vale {n*2}')
+print(f'O triplo de {n} vale {n*3}')
+print(f'A raiz quadrada de {n} é igual a {n**(1/2):.2f}')
+
