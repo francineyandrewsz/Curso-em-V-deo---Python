023a55f53406ceb107179ea2d704c348841ccaf3
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-06-30 21:16:00 -0300

    Tabuada

---
+++ DESAFIO009.py
@@ -0,0 +1,17 @@
+'''Faça um programa que leia um número inteiro qualquer e mostre na
+tela a sua tabuada.'''
+
+print('=>'*20, 'DESAFIO 009', '<='*20)
+tab = int(input('Digite um número para ver a sua tabuada: '))
+print('-'*15)
+print('{} X {:2} = {}'.format(tab, 1, tab*1))
+print('{} X {:2} = {}'.format(tab, 2, tab*2))
+print('{} X {:2} = {}'.format(tab, 3, tab*3))
+print('{} X {:2} = {}'.format(tab, 4, tab*4))
+print('{} X {:2} = {}'.format(tab, 5, tab*5))
+print('{} X {:2} = {}'.format(tab, 6, tab*6))
+print('{} X {:2} = {}'.format(tab, 7, tab*7))
+print('{} X {:2} = {}'.format(tab, 8, tab*8))
+print('{} X {:2} = {}'.format(tab, 9, tab*9))
+print('{} X {:2} = {}'.format(tab, 10, tab*10))
+print('-'*15)
