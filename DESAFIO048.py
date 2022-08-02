21030a0642f0aa3f684e94d95d2e446d415a612b
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-08-02 14:55:35 -0300

    Soma ímpares múltiplos de três

---
+++ DESAFIO048.py
@@ -0,0 +1,17 @@
+'''Faça um programa que calcule a soma entre todos os números
+ímpares que são múltiplos de três e que se encontram no intervalo 
+de 1 até 500.'''
+
+print('=>'*15, 'DESAFIO 048', '<='*15)
+
+soma = 0
+cont = 0
+
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        cont += 1
+        soma += c
+
+print(f'A soma de todos os {cont} valores solicitados é {soma}')
+
+    
