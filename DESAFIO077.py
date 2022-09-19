4b871d48d6d9138dfe88ad9643f9f79a65434b01
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-09-19 12:03:37 -0300

    Contando vogais em Tupla

---
+++ DESAFIO077.py
@@ -0,0 +1,12 @@
+'''Crie um programa que tenha uma tupla com várias palavras 
+(não usar acentos). Depois disso, você deve mostrar, 
+para cada palavra, quais são as suas vogais.'''
+
+print('=>'*15, 'DESAFIO 077', '<='*15)
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos: ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
+            
