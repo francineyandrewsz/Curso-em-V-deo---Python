cac801adbeaa143fce2d04fe2144452c42e4f266
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-10-25 12:03:23 -0300

    Boletim com listas compostas

---
+++ DESAFIO089.py
@@ -0,0 +1,29 @@
+'''Crie um programa que leia nome e duas notas de vários alunos e 
+guarde tudo em uma lista composta. No final, mostre um boletim contendo
+a média de cada um e permita que o usuário possa mostrar as notas 
+de cada aluno individualmente.'''
+print('=>'*15, 'DESAFIO 089', '<='*15)
+ficha = list()
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota1: '))
+    nota2 = float(input('Nota2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar?[S/N]')).upper()[0]
+    if resp in 'N':
+        break 
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-'*26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-'*35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe)\n'))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
+    
