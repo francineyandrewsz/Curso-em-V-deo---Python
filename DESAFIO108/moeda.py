6dc1155754c4b6fd02e11eabcd84a136580e02ce
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-12-16 21:57:54 -0300

    Formatando moedas

---
+++ DESAFIO108/moeda.py
@@ -0,0 +1,27 @@
+'''Adapte o código do desafio #107, criando uma função adicional 
+chamada moeda() que consiga mostrar os números como um valor
+monetário formatado.'''
+
+def aumentar(preco=0, taxa=0):
+    res = preco + (preco * taxa /100)
+    return res
+
+
+def diminuir(preco=0, taxa=0):
+    res = preco - (preco * taxa / 100)
+    return res
+
+
+def dobro(preco=0):
+    res = preco * 2
+    return res
+
+
+def metade(preco=0):
+    res = preco / 2
+    return res
+
+
+def moeda(preco=0, moeda='R$'):
+    return f'{moeda}{preco:>8.2f}'.replace('.', ',')
+
